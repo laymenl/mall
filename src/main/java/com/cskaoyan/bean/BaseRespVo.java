835b1d9aeaf6ede66e18b4590bba4d324cd2bfc1
cswangqian <js98bwq@163.com>
2020-09-30 23:29:03 +0800

    spring boot

--- src/main/java/com/cskaoyan/bean/BaseRespVo.java
+++ src/main/java/com/cskaoyan/bean/BaseRespVo.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 /**
  * Vo viewObject
- * @param <T>
- * 这是前后端分离，json数据一个常用的格式
+ *
+ * @param <T> 这是前后端分离，json数据一个常用的格式
  */
 @Data
 public class BaseRespVo<T> {
@@ -13,33 +13,37 @@ public class BaseRespVo<T> {
     String msg;//告诉前端请求的消息
     long errno;//自己的前后端应用 ： 自定义的状态码 → 通常前端根据该状态码做不同的处理
 
-    public static BaseRespVo ok(){
+    public static BaseRespVo ok() {
         BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         baseRespVo.setErrno(0);
         baseRespVo.setMsg("成功");
         return baseRespVo;
     }
-    public static BaseRespVo ok(Object data){
+
+    public static BaseRespVo ok(Object data) {
         BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         baseRespVo.setData(data);
         baseRespVo.setErrno(0);
         baseRespVo.setMsg("成功");
         return baseRespVo;
     }
-    public static BaseRespVo ok(Object data, String msg){
+
+    public static BaseRespVo ok(Object data, String msg) {
         BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         baseRespVo.setData(data);
         baseRespVo.setErrno(0);
         baseRespVo.setMsg(msg);
         return baseRespVo;
     }
-    public static BaseRespVo fail(){
+
+    public static BaseRespVo fail() {
         BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         baseRespVo.setErrno(500);
         baseRespVo.setMsg("失败");
         return baseRespVo;
     }
-    public static BaseRespVo fail(String msg){
+
+    public static BaseRespVo fail(String msg) {
         BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         baseRespVo.setErrno(500);
         baseRespVo.setMsg(msg);
