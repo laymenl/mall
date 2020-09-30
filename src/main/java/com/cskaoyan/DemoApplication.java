d1b848e666f6825ff9caeb71d175ece04bd1551b
byWhale <weijiangbai@qq.com>
2020-09-30 21:41:12 +0800

    commiter by byWhale

--- src/main/java/com/cskaoyan/DemoApplication.java
+++ src/main/java/com/cskaoyan/DemoApplication.java
@@ -1,8 +1,10 @@
-package com.cskaoyan.demo;
+package com.cskaoyan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cskaoyan.mapper")
 @SpringBootApplication
 public class DemoApplication {
 
