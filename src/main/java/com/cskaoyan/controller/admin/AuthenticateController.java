90a9d8391a3de6e1a473b555b7f189f41206b976
byWhale <648507447@qq.com>
2020-10-06 21:19:51 +0800

    readme update

--- src/main/java/com/cskaoyan/controller/admin/AuthenticateController.java
+++ src/main/java/com/cskaoyan/controller/admin/AuthenticateController.java
@@ -8,6 +8,7 @@ import com.cskaoyan.shiro.MallToken;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
