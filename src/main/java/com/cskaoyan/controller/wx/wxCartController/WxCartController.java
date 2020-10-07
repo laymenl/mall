1a8fb2daeb88f7945af4a5464ee761d674ac5944
wangjiawei <814670889@qq.com>
2020-10-07 12:05:56 +0800

    add cart update by wangjiawei

--- src/main/java/com/cskaoyan/controller/wx/wxCartController/WxCartController.java
+++ src/main/java/com/cskaoyan/controller/wx/wxCartController/WxCartController.java
@@ -1,12 +1,16 @@
 package com.cskaoyan.controller.wx.wxCartController;
 
 import com.cskaoyan.bean.BaseRespVo;
+import com.cskaoyan.bean.wxvo.cart.Cart;
 import com.cskaoyan.bean.wxvo.cart.CartListBean;
 import com.cskaoyan.service.wx.WxCartService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +26,14 @@ public class WxCartController {
         //System.out.println(username);
         CartListBean cartListBean = wxCartService.index(username);
         return BaseRespVo.ok(cartListBean);
+    }
+    /*productId: 125
+      goodsId: 1097011
+      number: 11
+      id: 227*/
+    @RequestMapping(value = "update",method = RequestMethod.POST)
+    public BaseRespVo update(@RequestBody Cart cart){
+        wxCartService.update(cart);
+        return BaseRespVo.ok();
     }
 }
