ec7db170ea131f357db266504f9aa587d90534c9
wangjiawei <814670889@qq.com>
2020-10-08 15:29:38 +0800

    add cart/add by wangjiawei

---
+++ src/main/java/com/cskaoyan/bean/wxvo/cart/AddBO.java
@@ -0,0 +1,37 @@
+package com.cskaoyan.bean.wxvo.cart;
+
+public class AddBO {
+    /**
+     * goodsId : 1083009
+     * number : 1
+     * productId : 98
+     */
+
+    private int goodsId;
+    private int number;
+    private int productId;
+
+    public int getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(int goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+}
